0510b33509fc6c4556c03893c1674400d0913427
drewp@bigasterisk.com <drewp@bigasterisk.com>
2019-12-24 20:30:27 -0800

    change to npm with a build that worked better as a local package to be shared

--- tasks.py
+++ tasks.py
@@ -3,35 +3,30 @@ from invoke import task  # pytype: disable=import-error
 
 @task
 def setup_npm(ctx):
-    ctx.run('yarn install')
+    ctx.run('npm run install')
 
 @task
 def serve_demo(ctx):
-    ctx.run('yarn webpack-dev-server')
+    ctx.run('npm run webpack-dev-server')
 
 @task
 def build(ctx):
-    ctx.run(f'yarn run webpack-build', pty=True)  # --debug --display-error-details
-    ctx.run(f'cp build/streamed-graph.bundle.js /my/site/homepage/www/rdf/streamed-graph.bundle.js')
-    ctx.run(f'cp src/streamed-graph.css         /my/site/homepage/www/rdf/streamed-graph.css')
+    ctx.run(f'npm run webpack-build', pty=True)
+    ctx.run(f'cp build/bundle.js         /my/site/homepage/www/rdf/streamed-graph.bundle.js')
+    ctx.run(f'cp src/streamed-graph.css  /my/site/homepage/www/rdf/streamed-graph.css')
 
 @task
 def build_forever(ctx):
-    ctx.run(f'yarn run webpack-build-forever', pty=True)
+    ctx.run(f'npm run run webpack-build-forever', pty=True)
 
 @task
 def dev_server(ctx):
-    ctx.run(f'yarn webpack-dev-server', pty=True)
+    ctx.run(f'npm run webpack-dev-server', pty=True)
 
 @task
 def test(ctx):
-    ctx.run(f'yarn test', pty=True)
+    ctx.run(f'npm run test', pty=True)
 
 @task
 def test_forever(ctx):
-    ctx.run(f'yarn test-forever', pty=True)
-
-# one time per machine:
-# yarn policies set-version v2
-# in vscode, ctrl-p then: ext install ark120202.vscode-typescript-pnp-plugin
-# or see https://next.yarnpkg.com/advanced/pnpify for a compatibility runner.
+    ctx.run(f'npm run test-forever', pty=True)
--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,34 @@
-const glob = require('glob');
-const jest = require('jest');
 const path = require("path");
-const PnpWebpackPlugin = require('pnp-webpack-plugin');
 
-const base = {
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: require.resolve('ts-loader'),
-                options: PnpWebpackPlugin.tsLoaderOptions({})
-            },
-            {
-                test: /\.css$/i,
-                use: ['file-loader']
-            },
-        ]
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-        plugins: [PnpWebpackPlugin],
-    }
+module.exports = {
+  entry: ["./src/index.ts"],
+  output: {
+    library: "streamed_graph",
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/build/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: require.resolve("ts-loader")
+      },
+      { test: /\.css$/i, use: ["file-loader"] }
+    ]
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".js"],
+    modules: ["node_modules"]
+  },
+  watchOptions: {
+    ignored: /node_modules/,
+    poll: 200
+  },
+  devServer: {
+    port: 8082,
+    publicPath: "/build/",
+    contentBase: __dirname
+  }
 };
-
-function outputToBundle(bundleName) {
-    return {
-        filename: bundleName,
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/build/'
-    };
-}
-
-module.exports = [
-    Object.assign({
-        name: "main",
-        entry: ['./src/streamed-graph.ts'],
-        output: outputToBundle('streamed-graph.bundle.js'),
-        devServer: {
-            port: 8082,
-            publicPath: '/build/',
-            contentBase: __dirname
-        }
-    }, base),
-    Object.assign({
-        name: "test",
-        entry: glob.sync('src/**/*.test.ts').map((p) => './' + p),
-        output: outputToBundle('test.bundle.js'),
-        plugins: [
-            {
-                apply: (compiler) => {
-                    compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
-                        jest.run([
-                            '--detectOpenHandles', // not just to debug; having this quiets a jest error
-                            '--testRegex', 'test.bundle.js', 'build/test.bundle.js']);
-                    });
-                }
-            }
-        ]
-    }, base)
-];
-
