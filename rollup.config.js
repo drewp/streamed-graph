07dc0a5a132fe463afdd77f79ef38877d023d48b
drewp@bigasterisk.com <drewp@bigasterisk.com>
2020-01-01 19:41:04 -0800

    sort imports

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import builtins from "rollup-plugin-node-builtins";
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
-import replace from '@rollup/plugin-replace';
 
 export default {
     input: "src/index.ts",
