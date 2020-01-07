3bf3f8d7e0add9c59aa4fe4c53de4a091a1f7762
drewp@bigasterisk.com <drewp@bigasterisk.com>
2020-01-06 23:52:30 -0800

    reformats and minor edits

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import builtins from "rollup-plugin-node-builtins";
-import commonjs from '@rollup/plugin-commonjs';
+import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 
